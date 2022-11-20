84d517e4a49061a365d6ecdcb102ed81655518d9
jacovaneeden <98823363+jacovaneeden@users.noreply.github.com>
2022-11-20 17:42:07 +0000

    Update untitled0.py

--- untitled0.py
+++ untitled0.py
@@ -6,3 +6,23 @@ Created on Sun Nov 20 17:11:14 2022
 @author: jacovaneeden
 """
 
+from scipy.integrate import odeint
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+t = np.linspace(0,15)
+K = 2
+h = 6
+
+dB = 5.6/(9.81*h)
+
+dO = K
+
+dT = dB - dO
+
+
+result = odeint(dT, 5, t)
+
+plt.plot(t,result)
+
