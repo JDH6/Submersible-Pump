9908bdbf4d2aa7f21d08744db3ae40f38834c316
MilesWeedon <98825066+MilesWeedon@users.noreply.github.com>
2022-11-24 23:46:15 +0000

    Create BoreholeMiles.py

---
+++ BoreholeMiles.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 24 19:09:08 2022
+
+@author: milesweedon
+"""
+
+from scipy.integrate import odeint
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+t = 0 #t = time period of running simulation in seconds
+
+
+waterTableHeight = 91.44 #initial water table height
+boreholeCrossSectionArea = 0.01824146925
+volumeOfPump = boreholeCrossSectionArea * 7.62
+verticalPipeCrossSectionArea = 5.067074791 * 10**(-4)
+boreholeFlowRateOut = 1.00944
+boreholeFlowRateIn = 0.1
+
+
+
+
+def volumeOfBorehole(waterTableHeight, boreholeCrossSectionArea, volumeOfPump, verticalPipeCrossSectionArea):
+    vol = (waterTableHeight * boreholeCrossSectionArea) - volumeOfPump - (verticalPipeCrossSectionArea*(waterTableHeight-25))
+    return vol
+    
+#print(verticalPipeCrossSectionArea)
+
+
+newVolumeAfterTime = 0
+
+while t < 3600:
+    netFlow = boreholeFlowRateIn - boreholeFlowRateOut
+    #print(netFlow)
+    newVolumeAfterTime = volumeOfBorehole(waterTableHeight, boreholeCrossSectionArea, volumeOfPump, verticalPipeCrossSectionArea) + (netFlow)
+    waterTableHeight = newVolumeAfterTime/(boreholeCrossSectionArea)
+    t+=1
+    #print(newVolumeAfterTime)
+    
+    
+    
