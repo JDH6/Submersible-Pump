0cc073631eb73eb39de35453463c39537fc66919
bennettphillip <98822466+bennettphillip@users.noreply.github.com>
2022-11-27 16:23:03 +0000

    Update pump_motor_capacity.py

--- pump_motor_capacity.py
+++ pump_motor_capacity.py
@@ -1,21 +1,28 @@
 import numpy as np
+
+#---------------CONSTANTS-------------------------------------------------
+
 rho = 1000 #water density
 g = 9.81 #gravitational constant
 pi = 3.14 #pi
-Rb = 7.62 #borehole radius (m)
-Rv = 1.27 #valve radius (m)
+
+#---------------SYSTEM CHARACTERISTICS------------------------------------
+
 t = np.linspace(0, 3600, num=3600, endpoint=False) #time is intervals to an hour, endpoint has been defined and not reached
-Pn = 5595 #nominal power
-Vn = 220 #nominal voltage
+Pn = 5595 #nominal power (W)
 es = 0.64 #starting pump efficiency
 el = 0.265 #lowered pump efficiency
-while waterTableHeight>=7.62:
+Rb = 0.1524 #borehole radius (m)
+Rv = 0.0254 #valve radius (m)
+
+#-------------------------------------------------------------------------
+
+while waterTableHeight(t)>=7.62:
     Pr = es*Pn #real power for submerged pump
-    Vr = es*Vn #real voltage for submervged pump
-    Vo = pi*waterTableHeight*(Rb**2-Rv**2) #Volume of borehole
+    Vb = pi*waterTableHeight*(Rb**2-Rv**2) #Volume of borehole
 else:
     Pr = el*Pn #real power for partially submerged pump
 Egp = rho*Vo*g*waterTableHeight #real voltage for partially submerged pump
-Ep = Pr*t #Electrical energy ouptu of motor
-if any(Ep) == Egp : #if electrical energy output equals energy to move water
-    endpoint=True #boolean true if motor output capacity equal to energy required to pump water
+Ep = Pr*t #Electrical energy ouptut of motor
+if any(Ep) <= Egp : #if electrical energy output less than or equal to energy to move water
+    endpoint=True #boolean value true, endpoint reached
