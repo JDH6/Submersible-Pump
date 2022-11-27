c10641155d921e8b17af60d73eea18fcc71e5b3f
MilesWeedon <98825066+MilesWeedon@users.noreply.github.com>
2022-11-27 00:07:29 +0000

    Fully working version

--- BoreholeMiles.py
+++ BoreholeMiles.py
@@ -6,42 +6,46 @@ Created on Thu Nov 24 19:09:08 2022
 @author: milesweedon
 """
 
-from scipy.integrate import odeint
 import numpy as np
 import matplotlib.pyplot as plt
 
 
+#Note that at time t = 0, the height is 300 ft above aquifier
+
+#---------------VARIABLES THAT WE EDIT---------------------------------------
+
 startTime = 0 
-endTime = 3600
+endTime = 3
+boreholeFlowRateOut = 5.0472 * 10**-3
+boreholeFlowRateIn = 1*10**-7
+boreholeDiameter = 0.1524
+
+#----------------------------------------------------------------------------
+
+initialWaterTableHeight = 91.44
+netFlow = boreholeFlowRateIn - boreholeFlowRateOut
+boreholeCrossSectionArea = np.pi * (boreholeDiameter / 2)**2
 
 
-
-waterTableHeight = 91.44 #initial water table height
-boreholeCrossSectionArea = 0.01824146925
+'''
 volumeOfPump = boreholeCrossSectionArea * 7.62
 verticalPipeCrossSectionArea = 5.067074791 * 10**(-4)
-boreholeFlowRateOut = 1.00944
-boreholeFlowRateIn = 0.1
+volumeOfPipe = verticalPipeCrossSectionArea * 83.82
+
+'''
 
 
+def waterTableHeight(t):
+    return initialWaterTableHeight + ((netFlow * t) / boreholeCrossSectionArea)
 
+xValues = []
+yValues = []                                      
 
-def volumeOfBorehole(waterTableHeight, boreholeCrossSectionArea, volumeOfPump, verticalPipeCrossSectionArea):
-    vol = (waterTableHeight * boreholeCrossSectionArea) - volumeOfPump - (verticalPipeCrossSectionArea*(waterTableHeight-25))
-    return vol
-    
-#print(verticalPipeCrossSectionArea)
+for t in range(startTime, endTime + 1):
+    xValues.append(t)
+    yValues.append(waterTableHeight(t))
 
-
-newVolumeAfterTime = 0
-
-while t < endTime:
-    netFlow = boreholeFlowRateIn - boreholeFlowRateOut
-    #print(netFlow)
-    newVolumeAfterTime = volumeOfBorehole(waterTableHeight, boreholeCrossSectionArea, volumeOfPump, verticalPipeCrossSectionArea) + (netFlow)
-    waterTableHeight = newVolumeAfterTime/(boreholeCrossSectionArea)
-    t+=1
-    #print(newVolumeAfterTime)
-    
-    
-    
+plt.plot(xValues,yValues, 'go--', linewidth=2, markersize=12)
+plt.xlabel('Time (seconds)')
+plt.ylabel('Water table height (metres)')
+plt.title('Water table height measured from the bottom of the borehole')
