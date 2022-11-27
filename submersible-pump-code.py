77274a3d3b08d24811597afa387ff1e0be206d7c
jacovaneeden <98823363+jacovaneeden@users.noreply.github.com>
2022-11-27 21:20:56 +0000

    Update submersible-pump-code.py

--- submersible-pump-code.py
+++ submersible-pump-code.py
@@ -11,15 +11,15 @@ import matplotlib.pyplot as plt
 
 N = int(input("Enter number of taps: "))
 T = int(input("Enter time period (in seconds): "))
+K = 0.01 #flow rate per tap
 
-def taps(t,N):
-    if t < 500:
-        K = 0.1
+def taps(t,N,k):
+    if t > 100 and t < 800:
+        K = k
     else:
         K = 0
     output = N * K
     return output
-
 
 Pressure_Initial_psi = 14.6959
 Pressure_Initial = Pressure_Initial_psi * 6894.76 #psi to pascals
@@ -27,16 +27,16 @@ TankVolume = 302.833
 Vlow = TankVolume - (Pressure_Initial * TankVolume) / 344738
 Vhigh = TankVolume - (Pressure_Initial * TankVolume) / 413685
 
-Flow_well_to_tank = 1.00944
+Flow_well_to_tank = 1.00944 #Litres per second
 
-print(Vlow,Vhigh)
+print('Lower Bound of Volume (litres): ',Vlow,',     Upper bound of Volume (litres): ',Vhigh)
 
 t = 0 #A counter for time that is independent from n
 Vl = [0] #Volume list with initial volume
 n = 0 #a counter for time
 V = 0 #Initial Volume 
-VfromWell = 0
-VfromWellInitial = 0
+VfromWell = 0 #Total volume pumped from well.
+VfromWellInitial = 0 #Volume from well when the pump turns off everytime after it has run
 
 while n < T:
     if V < 0: break
@@ -44,9 +44,10 @@ while n < T:
         t = 1
         Vstart = Vl[-1]
         while V <= Vhigh:
-            V = Vstart + ((Flow_well_to_tank - taps(n,N)) * t)
-            if V <= 0: break
-            VfromWell = VfromWellInitial + ((Flow_well_to_tank - taps(n,N)) * t) * 0.264172 #The total volume pumped from well converted to gallons
+            V = Vstart + ((Flow_well_to_tank - taps(n,N,K)) * t)
+            if V <= 0:
+                V = 0
+            VfromWell = VfromWellInitial + ((Flow_well_to_tank - taps(n,N,K)) * t) * 0.264172 #The total volume pumped from well converted to gallons
             t = t + 1
             Vl.append(V)
             n = n + 1
@@ -56,31 +57,20 @@ while n < T:
         t = 1
         Vstart = Vl[-1]
         while V > Vlow:
-            V = Vstart + ((- taps(n,N)) * t)
+            V = Vstart + ((- taps(n,N,K)) * t)
             t = t + 1
-            if V <= 0: break
+            if V <= 0:
+                V = 0
             Vl.append(V)
             n = n + 1
             if n >= T: break
             
-nList = np.linspace(0,n-1,n+1)
+nList = np.linspace(0,n-1,n+1) #List of n values
 
-if len(nList) == 0:
-    nEnd = 0
-else:
-    nEnd = int(nList[-1])
-
-if len(Vl) != T:
-    if nEnd == 0:
-        nList = [0]
-    for i in range(nEnd,T-1,1):
-        Vl.append(Vl[-1])
-    for i in range(nEnd,T-1,1):
-        nList = np.append(nList,i-nEnd)  
 
 Vl = [item * 0.264172 for item in Vl] #Converting litres to gallons
 
-plt.plot(nList,Vl)
+plt.plot(nList,Vl) #Plot the list of n against the list of Volumes
 plt.xlabel('Time (seconds)')
 plt.ylabel('Volume (gallons)')
 plt.show()
